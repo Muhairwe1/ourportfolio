b33244179ce46190c5751d7b30a561570161f1a1
josh-chief <yvoshngumi@gmail.com>
2023-02-24 12:23:42 +0300

    viewwork

---
+++ src/components/Viewwork.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <button>View Work</button>
+    </nav>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+button {
+    background-color: black;
+    color: aliceblue;
+
+}
+
+button:hover {
+    color: aqua;
+}
+</style>
